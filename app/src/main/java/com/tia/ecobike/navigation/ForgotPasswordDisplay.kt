7611bba54e25179eb3db0ea5db56540c53b30698
ferdiallen <ferdialif02@gmail.com>
2021-11-07 10:30:29 +0700

    Small updates

---
+++ app/src/main/java/com/tia/ecobike/navigation/ForgotPasswordDisplay.kt
@@ -0,0 +1,48 @@
+package com.tia.ecobike.navigation
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@ExperimentalFoundationApi
+@Composable
+fun ForgotDisplay() {
+    var newpassword by rememberSaveable {
+        mutableStateOf("")
+    }
+    val scope = rememberCoroutineScope()
+    val reloc = BringIntoViewRequester()
+    Column(
+        Modifier
+            .fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(26.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowBackIos,
+                contentDescription = "Back Arrow",
+                modifier = Modifier.align(Alignment.TopStart).padding(start = 20.dp,top = 8.dp)
+            )
+        }
+    }
+
+}
+
+@ExperimentalFoundationApi
+@Preview
+@Composable
+fun Previews() {
+    ForgotDisplay()
+}
